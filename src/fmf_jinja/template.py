0eeb7d47f62fd20da8225f2dfd8dd5f2c0371933
Cristian Le <cristian.le@mpsd.mpg.de>
2023-08-18 19:12:17 +0200

    Add template excludes
    Signed-off-by: Cristian Le <cristian.le@mpsd.mpg.de>

--- src/fmf_jinja/template.py
+++ src/fmf_jinja/template.py
@@ -1,21 +1,54 @@
+from __future__ import annotations
+
 import os
 import shutil
 from pathlib import Path
+
 from jinja2 import Environment, FileSystemLoader
 
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Union
+    from fmf.typing import DataType
 
 from fmf import Tree
+from fmf.normalize import normalize
+
+
+class Template:
+    path: Path
+    exclude: list[Path] = []
+
+    def __init__(self, data: DataType, root: str) -> None:
+        if type(data) == str:
+            self.path = normalize(Path, data, root, normalizer=self.normalize_path)
+            return
+        if isinstance(data, dict):
+            try:
+                path = data["path"]
+                if not isinstance(path, str):
+                    raise TypeError
+                self.path = normalize(Path, path, root, normalizer=self.normalize_path)
+                if "exclude" in data:
+                    self.exclude = normalize(list[Path], data["exclude"])
+                return
+            except TypeError as err:
+                raise TypeError(f"Unsupported input data: {data}") from err
+        else:
+            raise TypeError(f"Unsupported input data: [{type(data)}] {data}")
+
+    @staticmethod
+    def normalize_path(path_str: str, root: str) -> Path:
+        path = Path(path_str)
+        # Drop root ('/') to make it relative to tree.root
+        if path.is_absolute():
+            path = Path(*path.parts[1:])
+        # Create the absolute path including the tree.root
+        path = root / path
+        return path
 
 
 def _generate(tree: Tree, output: Path, output_root: Path) -> None:
-    # Get the template files/folders
-    templates: Union[str, list[str]] = tree["templates"]  # type: ignore[assignment]
-    if not isinstance(templates, list):
-        templates = [templates]
     # Get the vars used in the template
     tree_vars = tree.get("vars", {})
     assert isinstance(tree_vars, dict)
@@ -26,30 +59,34 @@ def _generate(tree: Tree, output: Path, output_root: Path) -> None:
     output.mkdir(parents=True, exist_ok=True)
     assert tree.root is not None
     # Loop over all templates files/folders
-    for tpl_str in templates:
-        tpl_path = Path(tpl_str)
-        # Drop root ('/') to make it relative to tree.root
-        if tpl_path.is_absolute():
-            tpl_path = Path(*tpl_path.parts[1:])
-        # Create the absolute path including the tree.root
-        tpl_path = tree.root / tpl_path
-        if not tpl_path.exists():
-            raise FileNotFoundError(f"Template path not found: {tpl_str} ({tpl_path})")
+    templates = tree.normalize(list[Template], "templates", tree.root)
+    assert isinstance(templates, list)
+    for template in templates:
+        if not template.path.exists():
+            raise FileNotFoundError(f"Template path not found: {template.path}")
         # Create the jinja environment that will load the
-        loader = FileSystemLoader(tpl_path if tpl_path.is_dir() else tpl_path.parent)
+        loader = FileSystemLoader(
+            template.path if template.path.is_dir() else template.path.parent
+        )
         env = Environment(loader=loader, keep_trailing_newline=True)
-        if tpl_path.is_dir():
+        if template.path.is_dir():
             # If it's a directory, loop over the file structure
-            for path_str, _, files_str in os.walk(tpl_path):
+            for path_str, dirs_str, files_str in os.walk(template.path):
                 # Generate the output path and make sure it exists
-                rel_path = Path(path_str).relative_to(tpl_path)
+                rel_path = Path(path_str).relative_to(template.path)
                 output_path = output / rel_path
                 output_path.mkdir(parents=True, exist_ok=True)
+                # Exclude all directories from recursive walk
+                for d in dirs_str:
+                    if rel_path / d in template.exclude:
+                        dirs_str.remove(d)
                 # Loop over all the files copying or rendering the templates
-                for fil in [Path(f) for f in files_str]:
+                for fil in [
+                    Path(f) for f in files_str if rel_path / f not in template.exclude
+                ]:
                     if ".j2" not in fil.suffixes:
                         # If it's not a template simply copy the file
-                        input_file = tpl_path / rel_path / fil
+                        input_file = template.path / rel_path / fil
                         output_file = output_path / fil
                         if input_file.is_symlink():
                             # If it's a symlink copy as is
@@ -76,10 +113,10 @@ def _generate(tree: Tree, output: Path, output_root: Path) -> None:
                             f.write(tpl.render(**tree_vars))
         else:
             # If it's a file treat it as a template and output it to the output root
-            tpl = env.get_template(tpl_path.name)
+            tpl = env.get_template(template.path.name)
             # Strip the .j2 suffix if it exists
             fil_name = ".".join(
-                [part for part in tpl_path.name.split(".") if part != "j2"]
+                [part for part in template.path.name.split(".") if part != "j2"]
             )
             output_file = output / fil_name
             with output_file.open("w") as f:
@@ -93,6 +130,8 @@ def _generate(tree: Tree, output: Path, output_root: Path) -> None:
         if link_path.is_absolute():
             link_path = output_root / Path(*link_path.parts[1:])
         output_link = output / link_name
+        # Makes sure paren directory is created
+        output_link.parent.mkdir(exist_ok=True)
         if output_link.is_symlink():
             # Remove existing symlink if it points to somewhere else
             if output_link.readlink() == link_path:
