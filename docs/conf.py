727fc1254ebf8c818ab0e1b77767b9d0b01658bf
Cristian Le <git@lecris.dev>
2024-11-14 10:59:58 +0100

    Quote style consistency

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ author = "Cristian Le"
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
 extensions = [
     "myst_parser",
